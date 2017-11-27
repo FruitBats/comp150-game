7877e931a4327d381ed227caa1df2cf2b2694c05
Tomas Mazurkevic <mazurkevic.tomas@gmail.com>
2017-11-27 14:42:31 +0000

    * Maintainability (Added doc-string to the Menu.py and some adjustments to other files)

--- FruitBats/HereBeDragons/Health.py
+++ FruitBats/HereBeDragons/Health.py
@@ -9,7 +9,8 @@ pygame.init()
 
 
 class CurrentHealth:
-    """Loads health bar images and updates it when being hit
+    """
+    Loads health bar images and updates it when being hit
     Now it only decreases upon key press.
     ToDo: set enemies's power AND
     scale red health bar accordingly to its maximum health
@@ -18,7 +19,7 @@ class CurrentHealth:
         health_bar (pygame.image): red health bar image
         health (pygame.image): green (current) player's health
         player_health: manually set player maximum health
-        """
+    """
     health_bar = pygame.image.load("ImageFiles/health_bar.png")
     health = pygame.image.load("ImageFiles/health.png")
     player_health = 100
